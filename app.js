125da4fb0e00feb96f75a3694fa627508ccf6220
Gaurav Gulati <83610860+Gulati-Gaurav@users.noreply.github.com>
2024-01-16 21:41:17 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ document.querySelector('.btn.btn-info').addEventListener('click', tool);
 document.querySelector('.btn.btn-dark').addEventListener('click', copy);
 
 
-function bracketRemove(e){
-
+function bracketRemove(){
     let input = document.querySelector('.form-control.input').value;
     let output = document.querySelector('.form-control.output');
+
     input = input.replaceAll("&gt;", ">");
     input = input.replaceAll("&lt;", "<");
     
     output.value = input;
-
-    // e.preventDefault();
 }
 
 function sr() {
     bracketRemove();
-    let input = document.querySelector('.form-control.input').value;
-    let output = document.querySelector('.form-control.output');
+    let input = document.querySelector('.form-control.output').value;
     let firstIndex = input.indexOf("<SearchResult>");
-    let lastIndex = input.indexOf("</SearchResult>");
+    let lastIndex = input.indexOf("</SearchResult>") + 15;
+
     console.log(firstIndex, lastIndex);
-    output.value = input;
-    if(firstIndex!=-1) output.value = input.substr(firstIndex, lastIndex+15);
+    let output = document.querySelector('.form-control.output');
+
+    firstIndex!=-1 ? output.value = input.substring(firstIndex, lastIndex) : output.value = input;
 }
 
 function tool() {
@@ -42,4 +41,4 @@ function copy() {
     navigator.clipboard.writeText(output.value);
 }
 
-// sdcsd &gt; sdj &lt; skdjv sd &gt; sd &lt; <SearchResult>  asddsfa </SearchResult>
+// sdcsd &gt; sdj &lt; skdjv sd &gt; sd &lt; <SearchResult>  asddsfa </SearchResult>sdcsd &gt; sdj &lt; skdjv sd &gt; sd &lt; <SearchResult>  asddsfa </SearchResult>
